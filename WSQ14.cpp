35c894200e8f016b2911cec6a42d92934afd0ef2
EduardoTorresB <lalotobu@hotmail.com>
2015-11-25 22:49:56 -0600

    Create WSQ14.cpp

---
+++ WSQ14.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+double calculateE(double n)
+{
+double x;
+double y;
+if(a==0)
+{
+x=1;
+return x;
+}
+c= 1+(1/n);
+b=pow(y,n);
+return x;
+}
+int main()
+{
+float n;
+cout << "Teclea un número " << endl;
+cin >> n;
+cout << "e =  " << calculateE(n) << endl;
+return 0;
+}
